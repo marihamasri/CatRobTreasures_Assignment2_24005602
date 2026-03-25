494ad5a4b6fcf68af252ece2deeb02c5fd163ad5
marihamasri <marihamasri06@gmail.com>
2026-03-25 22:08:08 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatRobTreasures_Assignment2_24005602
+{
+    public class Player
+    {
+        private string _playerId;
+        private string _name;
+        private int _level;
+
+        // Public Properties
+        public string Name
+        {
+            get { return _name; }
+            set { if (!string.IsNullOrEmpty(value)) _name = value; }
+        }
+
+        // Constructor
+        public Player(string id, string name, int level)
+        {
+            _playerId = id;
+            _name = name;
+            _level = level;
+        }
+
+        // Methods
+        public void Move(string direction)
+        {
+            Console.WriteLine($"{_name} moves {direction} stealthily.");
+        }
+
+        public void PickItem(string item)
+        {
+            Console.WriteLine($"{_name} picked up: {item}!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using CatRobTreasures_Assignment2_24005602;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player cat = new Player("P012", "Connie", 1);
+        Item treasure = new Item("I100", "Salmon Genie", 500);
+        Guard security = new Guard("Corgi", 10);
+
+        Console.WriteLine(" ^'_'^Game Start: Cat Rob Treasures ^'_'^ ");
+        security.Patrol();
+        cat.Move("Right");
+
+        Console.WriteLine("\nFinding Treasure...");
+        treasure.GetItem();
+        cat.PickItem("Salmon Genie");
+
+        Console.WriteLine("\nSecurity Check...");
+        security.DetectPlayer(true);
+        Console.WriteLine($"Current Guard Alert: {security.AlertLevel}%");
+    }
+}
