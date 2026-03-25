fe536a3b62adfdda02a50751cc2cc3d960e8102f
marihamasri <marihamasri06@gmail.com>
2026-03-25 22:38:52 +0800

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatRobTreasures_Assignment2_24005602
+{
+    public class Item
+    {
+        private string _itemId;
+        private string _itemType;
+        private int _value;
+
+        public string ItemType => _itemType;
+        public int Value => _value;
+
+        public Item(string id, string type, int val)
+        {
+            _itemId = id;
+            _itemType = type;
+            _value = val;
+        }
+
+        public void GetItem()
+        {
+            Console.WriteLine($"Item Type: {_itemType} | Value: {_value} gold.");
+        }
+
+        public void UseItem()
+        {
+            Console.WriteLine($"Using {_itemType} to bypass obstacle.");
+        }
+    }
+}
