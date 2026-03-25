47ce6a77d50ebb733c82ce21a23bbc56c3af421a
marihamasri <marihamasri06@gmail.com>
2026-03-25 22:36:28 +0800

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatRobTreasures_Assignment2_24005602
+{
+    internal class Class3
+    {
+    }
+}
